465a7575e6e751028eb7438b4f8f21c2d5d0f362
Malik Rahey <55609801+malikrahey@users.noreply.github.com>
2025-04-09 16:14:07 -0230

    Create F:/Projects/capylanga/screens/TestScreen.styles.js

---
+++ F:/Projects/capylanga/screens/TestScreen.styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  button: {
+    backgroundColor: '#6200ee',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
