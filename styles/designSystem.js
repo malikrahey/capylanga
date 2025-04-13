e69b4fda9fbc88f30618618f8a789795731f7c0d
Malik Rahey <55609801+malikrahey@users.noreply.github.com>
2025-04-13 17:47:27 -0230

    Create styles/designSystem.js

---
+++ styles/designSystem.js
@@ -0,0 +1,25 @@
+export const colors = {
+  primary: '#4F46E5',
+  background: '#F9FAFB',
+  text: '#1F2937',
+  white: '#FFFFFF',
+};
+
+export const fonts = {
+  size: {
+    small: 14,
+    medium: 16,
+    large: 20,
+  },
+  weight: {
+    normal: '400',
+    bold: '700',
+  },
+  lineHeight: 24,
+};
+
+export const spacing = {
+  small: 8,
+  medium: 16,
+  large: 24,
+};
