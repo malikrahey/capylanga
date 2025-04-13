35e2c307c53d976f42917ddd25ec2a2230275a08
Malik Rahey <55609801+malikrahey@users.noreply.github.com>
2025-04-13 18:40:30 -0230

    Create styles/lessonStyles.js

---
+++ styles/lessonStyles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  contentContainer: {
+    marginBottom: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  progressContainer: {
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: '#6200ee',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
