3d85863ea6e21fcfa35772fd769dfd0925f3fde4
Malik Rahey <55609801+malikrahey@users.noreply.github.com>
2025-05-01 20:12:36 -0230

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./screens/home/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./screens/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-50': '#eff6ff',
+        'blue-100': '#dbeafe',
+        'blue-500': '#3b82f6',
+        'blue-800': '#1e40af',
+        'gray-100': '#f3f4f6',
+        'gray-700': '#374151',
+        'gray-800': '#1f2937',
+      },
+    },
   },
   plugins: [],
-}
-
+};
